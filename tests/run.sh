b78f44df0057f4144c9339de25c39cb2a812178b
mchalczynski <marcin.chalczynski@rndity.com>
2017-04-06 14:18:32 +0200

    tests: increase sleep in tests/run.sh (10s)
    Issues: MEN-1119
    
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/run.sh
+++ tests/run.sh
@@ -2,6 +2,6 @@
 cd testing
 
 # if we're running in a container, wait a little before starting tests
-[ $$ -eq 1 ] && sleep 5
+[ $$ -eq 1 ] && sleep 10
 
 py.test-3 -s --tb=short --api=0.1.0  --host mender-inventory:8080 --verbose --junitxml=results.xml tests/
