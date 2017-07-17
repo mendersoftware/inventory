4e65df772eab481d158d75e0e38786ec8bb6bc7b
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-07-17 13:25:48 +0200

    tests: update acceptance tests setup to support absolute paths and logging
    Add necessary supposrt to acceptance tests infra so that inventory acceptance
    tests can be run the same way as for other services.
    
    Changelog: none
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>
    
    Changelog: None

--- tests/run.sh
+++ tests/run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-cd testing
+
+# tests are supposed to be located in the same directory as this file
+
+DIR=$(readlink -f $(dirname $0))
+
+export PYTHONDONTWRITEBYTECODE=1
+
+HOST=${HOST="mender-inventory:8080"}
 
 # if we're running in a container, wait a little before starting tests
 [ $$ -eq 1 ] && {
     echo "-- running in container, wait for other services"
+    # wait 10s for containters to start and
     sleep 10
 }
 
-py.test-3 -s --tb=short --api=0.1.0  --host mender-inventory:8080 --verbose --junitxml=results.xml tests/
+py.test-3 -s --tb=short --host $HOST \
+          --management-spec $DIR/management_api.yml \
+          --verbose --junitxml=$DIR/results.xml \
+          --inventory-items $DIR/inventory_items \
+          $DIR/tests/test_*.py "$@"
+
