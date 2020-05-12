b888fcf935ccf3670b15b3bf3629e855e8db20ed
Ole Petter <ole.orhagen@northern.tech>
2020-05-12 08:06:13 +0200

    chore(license_test.go): Conform to the new mendertesting API
    Changelog: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- license_test.go
+++ license_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Northern.tech AS
+// Copyright 2020 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -18,5 +18,5 @@ import "testing"
 import mt "github.com/mendersoftware/mendertesting"
 
 func TestLicenses(t *testing.T) {
-	mt.CheckLicenses(t)
+	mt.CheckMenderCompliance(t)
 }
